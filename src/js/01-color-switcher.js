e9b2a546819d3819df005eaf947ce0b7c08502e3
Jenny85185 <67415254+Jenny85185@users.noreply.github.com>
2022-09-10 16:29:40 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,23 +4,27 @@ const btnStop = document.querySelector('[data-stop]');
 
 btnStart.addEventListener('click', clickOnBtnColor);
 btnStop.addEventListener('click', clickOfBtnColor);
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 let timerId = null;
 
 function clickOnBtnColor() {
   btnStart.disabled = true;
   btnStop.disabled = false;
-  setTimeout(() => {
+
+  timerId = setInterval(() => {
     const colorRandom = getRandomHexColor();
     bBody.style.background = colorRandom
-  },1000);
-  
-    
-}
+  }, 1000)
+ 
+} 
+
 function clickOfBtnColor() {
   btnStart.disabled = false;
   btnStop.disabled = true;
   clearInterval(timerId);
-}
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
